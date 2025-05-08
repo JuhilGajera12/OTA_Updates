489bfd66ea80a2bce0adc89583b29780a8fa3548
JuhilGajera12 <juhil.gajera@novicbase.com>
2025-05-08 11:39:28 +0530

    Setup for Over air updates

--- App.tsx
+++ App.tsx
@@ -1,58 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useEffect} from 'react';
 import {
-  ScrollView,
+  Alert,
+  LayoutAnimation,
+  Platform,
   StatusBar,
-  StyleSheet,
   Text,
   useColorScheme,
   View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import hotUpdate from 'react-native-ota-hot-update';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -61,71 +18,88 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the recommendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
+  useEffect(() => {
+    const onCheckGitVersion = () => {
+      hotUpdate.git.checkForGitUpdate({
+        branch: Platform.OS === 'ios' ? 'main' : 'main',
+        bundlePath:
+          Platform.OS === 'ios'
+            ? 'output/main.jsbundle'
+            : 'output/index.android.bundle',
+        url: 'https://github.com/JuhilGajera12/OTA_Updates.git',
+        onCloneFailed(msg: string) {
+          Alert.alert('Clone project failed!', msg, [
+            {
+              text: 'Cancel',
+              onPress: () => {},
+              style: 'cancel',
+            },
+          ]);
+        },
+        onCloneSuccess() {
+          Alert.alert(
+            'Clone project success!',
+            'Restart to apply the changes',
+            [
+              {
+                text: 'OK',
+                onPress: () => hotUpdate.resetApp(),
+              },
+              {
+                text: 'Cancel',
+                onPress: () => {},
+                style: 'cancel',
+              },
+            ],
+          );
+        },
+        onPullFailed(msg: string) {
+          Alert.alert('Pull project failed!', msg, [
+            {
+              text: 'Cancel',
+              onPress: () => {},
+              style: 'cancel',
+            },
+          ]);
+        },
+        onPullSuccess() {
+          Alert.alert('Pull project success!', 'Restart to apply the changes', [
+            {
+              text: 'OK',
+              onPress: () => hotUpdate.resetApp(),
+            },
+            {
+              text: 'Cancel',
+              onPress: () => {},
+              style: 'cancel',
+            },
+          ]);
+        },
+        onProgress(received: number, total: number) {
+          const percent = (+received / +total) * 100;
+          LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+        },
+        onFinishProgress() {
+          console.log('onFinishProgress');
+        },
+      });
+    };
+    onCheckGitVersion()
+  }, []);
 
   return (
-    <View style={backgroundStyle}>
+    <View
+      style={[
+        backgroundStyle,
+        {flex: 1, justifyContent: 'center', alignItems: 'center'},
+      ]}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <Text style={{fontSize: 22, color: 'white'}}>Magan</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
